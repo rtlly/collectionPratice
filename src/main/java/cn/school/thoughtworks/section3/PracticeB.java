0d9d516650ca2d52bd675cc991e1cd79a4f74228
Li Yu <yszli@thoughtworks.com>
2018-04-17 22:30:39 +0800

    根据测试完成了 section3 中 PraticeB 的实现，并测试通过

--- src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -6,7 +6,15 @@ import java.util.Map;
 public class PracticeB {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        for(String key : collectionA.keySet()){
+            int j = 0;
+            while(j < object.get("value").size() && !key.equals(object.get("value").get(j))){
+                ++j;
+            }
+            if(j < object.get("value").size()){
+                collectionA.replace(key,collectionA.get(key)-collectionA.get(key)/3);
+            }
+        }
+        return collectionA;
     }
 }
