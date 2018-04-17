cbe08ff0f308bacb15fbd8045cb0c7eed9b843b8
Li Yu <yszli@thoughtworks.com>
2018-04-17 22:21:48 +0800

    根据测试完成了 section1 中 PraticeC 的实现，并测试通过

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,11 +1,22 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String> same = new ArrayList<>();
+        for(int i = 0;i < collection1.size();++i){
+            int j = 0;
+            while(j < collection2.get("value").size() && collection1.get(i) != collection2.get("value").get(j)){
+                ++j;
+            }
+            if(j < collection2.get("value").size()){
+                same.add(collection1.get(i));
+            }
+        }
+        return same;
     }
 }
