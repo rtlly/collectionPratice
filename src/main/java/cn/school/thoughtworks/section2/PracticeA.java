51ebad740d6b0e5b5a10756f08feb63fb9305472
Li Yu <yszli@thoughtworks.com>
2018-04-17 22:26:24 +0800

    根据测试完成了 section1 中 PraticeA 的实现，并测试通过

--- src/main/java/cn/school/thoughtworks/section2/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeA.java
@@ -1,12 +1,20 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeA {
     Map<String,Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String,Integer> same = new HashMap<>();
+        for(int i = 0; i < collection1.size();++i){
+            if(same.containsKey(collection1.get(i))){
+                same.put(collection1.get(i),same.get(collection1.get(i))+1);
+            }else{
+                same.put(collection1.get(i),1);
+            }
+        }
+        return same;
     }
 }
