0b99e52af001577ca6a70c101f335fbad91fc5a8
Li Yu <yszli@thoughtworks.com>
2018-04-17 22:19:38 +0800

    根据测试完成了 section1 中 PraticeA 的实现，并测试通过

--- src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -1,10 +1,21 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String> same = new ArrayList<>();
+        for(int i = 0; i < collection1.size();++i){
+            int j = 0;
+            while(j < collection2.size() && collection1.get(i) != collection2.get(j)){
+                ++j;
+            }
+            if(j < collection2.size()){
+                same.add(collection1.get(i));
+            }
+        }
+        return same;
     }
 }
