81219386f03538ed803b1f0f89cb8ded4ba49a29
Li Yu <yszli@thoughtworks.com>
2018-04-17 22:31:02 +0800

    根据测试完成了 section3 中 PraticeC 的实现，并测试通过

--- src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -1,12 +1,29 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String,Integer> same = new HashMap<>();
+        for(int i = 0; i < collectionA.size();++i){
+            if(same.containsKey(collectionA.get(i))){
+                same.put(collectionA.get(i),same.get(collectionA.get(i))+1);
+            }else{
+                same.put(collectionA.get(i),1);
+            }
+        }
+        for(String key : same.keySet()){
+            int j = 0;
+            while(j < object.get("value").size() && !key.equals(object.get("value").get(j))){
+                ++j;
+            }
+            if(j < object.get("value").size()){
+                same.replace(key,same.get(key)-same.get(key)/3);
+            }
+        }
+        return same;
     }
 }
