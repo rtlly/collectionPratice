26c02051c862a80a767373f2723abbe0e0bef2e1
Li Yu <yszli@thoughtworks.com>
2018-04-17 22:30:14 +0800

    根据测试完成了 section3 中 PraticeA 的实现，并测试通过

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -1,12 +1,21 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeA {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        for(String key : collectionA.keySet()){
+            int j = 0;
+            while(j < object.get("value").size() && !key.equals(object.get("value").get(j))){
+                ++j;
+            }
+            if(j < object.get("value").size()){
+                collectionA.replace(key,collectionA.get(key)-1);
+            }
+        }
+        return collectionA;
     }
 }
